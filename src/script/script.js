900a0f71767a4d386d6219dd600a233650899c06
Emanuel_06 <emanuelgds2006@gmail.com>
2026-05-11 22:15:48 -0300

    refactor: image optimization, visual adjustments and general fixes

--- src/script/script.js
+++ src/script/script.js
@@ -3,21 +3,23 @@ const headerContainer = document.querySelector(".header__container")
 const toggle = document.querySelector(".header__toggle")
 const nav = document.querySelector(".nav")
     
+
     toggle.addEventListener("click", () => {
         toggle.classList.toggle("ativa")
         nav.classList.toggle("ativa")
         headerContainer.classList.toggle("ativa")
     
-    })
+    });
 
 // NAV OCULT 
 const navLinks = document.querySelectorAll(".nav__link");
 
+
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
-            nav.classList.remove("ativa");
+            nav.classList.remove("ativa")
             toggle.classList.remove("ativa")
-            headerContainer.classList.remove("ativa");
+            headerContainer.classList.remove("ativa")
 
-        })
+        });
     });
